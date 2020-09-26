f75ba5d5bfc57fb4a40b37948f2b8fe947cc5373
Andrew672 <amarbach5@gmail.com>
2020-09-26 23:29:45 +0200

    ajout commande !mute et !unmute

--- commands/mute.js
+++ commands/mute.js
@@ -1,18 +1,21 @@
 module.exports = 
 {
-    name:'mute',
-    aliases :['mutes'] ,
-    run:async(bot,message,args)=> 
+  name:'mute',
+  aliases :['mutes'] ,
+  run:async(bot,message,args)=> 
+  {
+    if (message.member.voice.channel) 
     {
-        if (message.member.voice.channel) {
-            let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-            for (const [memberID, member] of channel.members) 
-            {
-              member.voice.setMute(true);
-            }
-          } else {
-            message.reply('Vous devez être dans un channel vocal !!!');
-          }
+      let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+      for (const [memberID, member] of channel.members) 
+      {
+        member.voice.setMute(true);
+      }
+    } 
+    else 
+    {
+      message.reply('Vous devez être dans un channel vocal !!');
+    }
           
-     }
+  }
 }
--- commands/unmute.js
+++ commands/unmute.js
@@ -1,17 +1,21 @@
 module.exports = 
 {
-    name:'unmute',
-    aliases :['unmutes'] ,
-    run:async(bot,message,args)=> 
+  name:'unmute',
+  aliases :['unmutes'] ,
+  run:async(bot,message,args)=> 
+  {
+    if (message.member.voice.channel) 
     {
-        if (message.member.voice.channel) {
-            let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-            for (const [memberID, member] of channel.members) {
-              member.voice.setMute(false);
-            }
-          } else {
-            message.reply('Vous devez être dans un channel vocal !!!');
-          }
+      let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+      for (const [memberID, member] of channel.members)
+      {
+        member.voice.setMute(false);
+      }
+    } 
+    else 
+    {
+      message.reply('Vous devez être dans un channel vocal !!');
+    }
           
-     }
+  }
 }
