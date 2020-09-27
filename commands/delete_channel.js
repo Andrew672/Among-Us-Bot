b91f96cf4558885003d5b868275edc96e6e16456
Andrew672 <amarbach5@gmail.com>
2020-09-27 18:43:07 +0200

    Création de la commande permettant de supprimer un channel avec son id

---
+++ commands/delete_channel.js
@@ -0,0 +1,17 @@
+module.exports = 
+{
+  name:'del',
+  aliases :['dels'] ,
+  run:async(bot,message,args)=> 
+    {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) {message.channel.send("Désolé, vous n'avez pas la permission !")}
+        else 
+        {
+            let fetchedChannel = message.guild.channels.cache.find(r => r.id === args[0]);
+            fetchedChannel.delete(); 
+        }
+           
+    
+    }
+          
+}
