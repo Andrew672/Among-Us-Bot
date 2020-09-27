46471f0803edc07486d515f4a0a03e5e224ff5ac
Andrew672 <amarbach5@gmail.com>
2020-09-27 19:46:02 +0200

    Ajout commande !del

--- commands/delete_channel.js
+++ commands/delete_channel.js
@@ -4,7 +4,7 @@ module.exports =
   aliases :['dels'] ,
   run:async(bot,message,args)=> 
     {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) {message.channel.send("Désolé, vous n'avez pas la permission !")}
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) {message.channel.send("Désolé, vous n'avez pas la permission !!")}
         else 
         {
             let fetchedChannel = message.guild.channels.cache.find(r => r.id === args[0]);
